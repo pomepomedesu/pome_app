e4495e27a8c06923f97b0839e74d3b38b16a74d0
pomepomedesu <kaitopoohlove@gmail.com>
2020-09-25 19:28:44 +0900

    push

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ working_directory "#{app_path}/current"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
+#ポート番号を指定 
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
